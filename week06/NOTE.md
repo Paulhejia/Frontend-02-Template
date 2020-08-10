efbdb4eb2b349f104f431cd1d123074f60e85704
hejiahui <1016398426@qq.com>
2020-08-10 23:47:26 +0800

    week06 match函数成功

--- week06/NOTE.md
+++ week06/NOTE.md
@@ -78,3 +78,5 @@ Note that the ::first-letter element is inside the ::first-line element. Propert
 
 ```
 我只是猜测 ::first-line 是基于 ::first-letter 生成的（根执行顺序有关），样式也是要继承于 first-letter, 而且first-line 如果也能设置 float 盒模型，这样样式也很难控制了
+
+first-letter是在布局完成之后，确定了一段文字中的第一个文字，可以对其操作布局时性能开销小； 而first-line选中的是第一行文字，不同的宽度选中的文字内容不一样，要对其重新布局排版消耗性能大,所以first-letter 可以设置 float 之类的，而 first-line 不行。
--- week06/src/matchSelector.js
+++ week06/src/matchSelector.js
@@ -1,174 +1,172 @@
 // 匹配css
-const parser = require('postcss-selector-parser');
-// const normalized = parser().processSync('h1, h2, h3', {lossless: false});
-const spaceReg = /^[\t\f\n ]$/;
-const tagNameReg = /^[a-zA-Z]$/;
 
-var selectNodes = []
-var currentSelector = {};
+const spaceReg = /^[\t\f\n ]$/;
+const tagNameReg = /^[a-zA-Z\-*]$/;
+let currentDom = null;
+
+// 获取选择器
+// type: class id combination 
+// value: 值
+function getSelectors(selector) {
+    var selectNodes = []
+    var currentSelector = {};
+    function start(s) {
+        if(s.match(spaceReg)) {
+            emit({
+                type: 'combinator',
+                value: ' ',
+            })
+            endSelect()
+            return start;
+        }
+        else if(s === '#'){
+            emit({
+                type: 'id',
+                value: '',
+            })
+            return startSelect;
+        }
+        else if(s === '.'){
+            emit({
+                type: 'class',
+                value: '',
+            })
+            return startSelect;
+        }
+        else if(s.match(tagNameReg)) {
+            emit({
+                type: 'tag',
+                value: s,
+            })
+            return selectName;
+        }
+    }
+    function endOfFinal() {
+        endSelect();
+    }
+    function startSelect(s) {
+        if(s.match(tagNameReg)) {
+            return selectName(s);          
+        }
+        else{
+            throw new Error('错误的selector')
+        }            
+    }
+    function endSelect() {
+        selectNodes.push(currentSelector);
+        return start;
+        // currentSelector = {};
+    }
+    function selectName(s) {
+        // 结束
+        if(s.match(tagNameReg)) {
+            currentSelector.value = currentSelector.value + s;
+            return selectName;
+        }            
+        else{                
+            endSelect();
+            return start(s);
+        }  
+    }
+    function emit(token) {
+        currentSelector = {
+            value: token.value,
+            type: token.type
+        }
+    }
+    let state = start        
+    for(var s of selector) {
+        state = state(s)
+    }
+    endOfFinal();
+    return selectNodes;
+}
+/*
+* @params{selector} Object 选择器描述对象 { type: 'xxx', value: 'xxx' }
+* @params{isTarget} Boolean  是否目标元素 document.getElementById("id")
+*
+**/ 
+function matchSelector(selector, isTarget) {
+    const { type, value } = selector
+    if(type == 'tag' ) {            
+        if(currentDom.tagName.toLowerCase() === value) {
+            return true;
+        }
+        else {
+            if(currentDom.parentElement && !isTarget) {
+                currentDom = currentDom.parentElement                
+                return matchSelector(selector);
+            }else {
+                return false;
+            }                
+        }
+    }
+    else if(type == 'class') {
+        if(currentDom.classList.contains(value)) {
+            return true;
+        }
+        else {
+            if(currentDom.parentElement && !isTarget) {
+                currentDom = currentDom.parentElement
+                return matchSelector(selector);
+            }else {
+                return false;
+            }                
+        }
+    }
+    else if(type == 'id') {
+        if(currentDom.id === value) {
+            return true;
+        }
+        else {
+            if(currentDom.parentElement && !isTarget) {
+                currentDom = element.parentElement;
+                return matchSelector(selector)
+            }else {
+                return false;
+            }                
+        }
+    }
+}
 
 window.onload = function() {
     
     function match(selector, element) {
-        
-        function start(s) {
-            if(s.match(spaceReg)) {
-                emit({
-                    type: 'combinator',
-                    value: ' ',
-                })
-                endSelect()
-                return start;
-            }
-            else if(s === '#'){
-                emit({
-                    type: 'id',
-                    value: '',
-                })
-                return startSelect;
-            }
-            else if(s === '.'){
-                emit({
-                    type: 'class',
-                    value: '',
-                })
-                return startSelect;
-            }
-            else if(s.match(tagNameReg)) {
-                emit({
-                    type: 'tag',
-                    value: s,
-                })
-                return selectName;
-            }
-        }
-        function endOfFinal() {
-            endSelect();
-        }
-        function startSelect(s) {
-            if(s.match(tagNameReg)) {
-                return selectName(s);          
-            }
-            else{
-                throw new Error('错误的selector')
-            }            
-        }
-        function endSelect() {
-            selectNodes.push(currentSelector);
-            return start;
-            // currentSelector = {};
-        }
-        function selectName(s) {
-            // 结束
-            if(s.match(tagNameReg)) {
-                currentSelector.value = currentSelector.value + s;
-                return selectName;
-            }            
-            else{                
-                endSelect();
-                return start(s);
-            }  
-        }
-        function emit(token) {
-            currentSelector = {
-                value: token.value,
-                type: token.type
-            }
-        }
-        let state = start        
-        for(var s of selector) {
-            state = state(s)
-        }
-        endOfFinal();
-
-         // 先找出所有的父节点，再找出所有符合的字节点，
-    let filterParents = [];
-    let currentChildrens = [];
-    let currentDom = element
-    function mainProcess() {        
-        for(let i in selectNodes) {
-            const cSelector = selectNodes[i]
-            const {type, value} = cSelector      
-            if(i == 0) {
-                findParent(cSelector, element)
-            }
-            debugger  ;
-            filterParents
-            // else if(type == 'combinator') { 
-            //     const childrens = currentTargetDom = currentTargetDom.childNodes;
-            // }
-        }
-    }
-    mainProcess();
-    
-    function findParent(selector, element) {
-        const {type, value} = selector
-        if(type == 'tag' ) {            
-            if(element.parentElement.tagName.toLowerCase() === value) {
-                filterParents.push(element.parentElement)    
-                return findParent(selector, element.parentElement)
+        let selectNodes = getSelectors(selector);     
+        console.log('selectNodes:', selectNodes);   
+        let result = true;
+        let _arr = selectNodes.reverse();
+        let isElTarget = true;
+        // 从自生往上面找
+        currentDom = element     
+        for(let _selector of _arr) {         
+            if(_selector.type === 'combinator') {
+                isElTarget = false;
+                if(currentDom.parentElement) {
+                    currentDom = currentDom.parentElement
+                    continue;
+                }
+                else {
+                    result = false;
+                    break
+                }                    
             }
             else {
-                if(filterParents.length) {
-                    return true;
-                }else {
-                    return false;
-                }                
-            }
+                result = matchSelector(_selector, isElTarget);
+                if(result) {                        
+                    continue
+                }
+                else {
+                    break;
+                }
+            }                               
         }
-        else if(type == 'class') {
-            if(element.parentElement.classList.contains(value)) {
-                filterParents.push(filterParents)    
-                return findParent(selector, element)
-            }
-            else {
-                if(filterParents.length) {
-                    return true;
-                }else {
-                    return false;
-                }                
-            }
-        }
-        else if(type == 'id') {
-            if(element.parentElement.id === value) {
-                filterParents.push(filterParents)    
-                return findParent(selector, element)
-            }
-            else {
-                if(filterParents.length) {
-                    return true;
-                }else {
-                    return false;
-                }                
-            }
-        }
-        else if(type == 'combinator') { 
-            // const childrens = currentTargetDom = currentTargetDom.childNodes;
-        }
-    }
-    function findChildrens(selector) {
-        if(type == 'tag') {
-
-        }
-        else if(type == 'class') {
-
-        }
-        else if(type == 'id') {
-
-        }
-        else if(type == 'combinator') { 
-            const childrens = currentTargetDom = currentTargetDom.childNodes;
-        }
-        return true;
-    }
-
-
-
+        console.log('result:', result)         
     }
     
-    match("div #id.class", document.getElementById("id"));
-    console.log('selectNodes:', selectNodes);
-   
+    match("html #id.abc", document.getElementById("id")); // 预期 false
+    match("div #id.class", document.getElementById("id")); // 预期 true
+    match("body #id .test-letter", document.querySelector('.test-letter')); // 预期 true
 }
 
 
